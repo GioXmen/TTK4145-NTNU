3df610581208570e86c1f4b147cc55f6fc651c11
Ernestas <ernestas1@yandex.ru>
2019-01-18 17:00:16 +0100

    Go

--- Exercise 2/Part2/go/foo.go
+++ Exercise 2/Part2/go/foo.go
@@ -1,6 +1,3 @@
-
-// Use `go run foo.go` to run your program
-
 package main
 
 import (
@@ -9,15 +6,11 @@ import (
 	"time"
 )
 
-// Control signals
-
 func number_server(add_number <-chan int, number chan<- int) {
 	var i = 0
-	// This for-select pattern is one you will become familiar with if you're using go "correctly".
 
 	for {
 		select {
-
 		case a := <-add_number:
 			if a == -1 {
 				i--
@@ -26,28 +19,17 @@ func number_server(add_number <-chan int, number chan<- int) {
 				i++
 			}
 
-
-			// TODO: receive different messages and handle them correctly
-			// You will at least need to update the number and handle control signals.
-
-
-
 		case number <- i:
-
 	}
 
 }
-
 }
 
 func incrementing(add_number chan<- int, finished chan bool) {
 	for j := 0; j < 1000000; j++ {
 		add_number <- 1
 	}
-
 	finished <- true
-
-	//TODO: signal that the goroutine is finished
 }
 
 func decrementing(add_number chan<- int, finished chan bool) {
@@ -55,24 +37,14 @@ func decrementing(add_number chan<- int, finished chan bool) {
 		add_number <- -1
 	}
 	finished <- true
-	//TODO: signal that the goroutine is finished
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// TODO: Construct the required channels
-
 	ch_change_i := make(chan int)
-
 	ch_i_value := make(chan int)
-
 	ch_finished := make(chan bool)
-	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
-
-	// TODO: Spawn the required goroutines
-
-	// TODO: block on finished from both "worker" goroutines
 
 	go incrementing(ch_change_i, ch_finished)
 	go decrementing(ch_change_i, ch_finished)
